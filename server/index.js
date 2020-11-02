572ec59fd91e56259106796da457b3c0e893b864
twzkraus <twzkraus@gmail.com>
2020-11-02 15:06:47 -0800

    removes console-logging

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('dist'));
 
 app.get('/stats', (req, res) => {
-  console.log(req.query);
   executeRequest(req.query)
     .then(response => {
       res.send(response.data);
