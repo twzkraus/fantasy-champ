4bc54e8597b2379447b4a9f6fb6d7d97ec3d8306
twzkraus <twzkraus@gmail.com>
2020-11-03 17:18:13 -0800

    updates port for heroku deploy

--- server/index.js
+++ server/index.js
@@ -17,6 +17,6 @@ app.get('/stats', (req, res) => {
     })
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Express Server is listening on port ${PORT}`);
 });
