59d16ffcafbffce97947ab1a7c46a6ab2d02f3f4
twzkraus <twzkraus@gmail.com>
2020-11-03 17:04:21 -0800

    turns off watch tag for webpack. Moves to production mode.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  watch: true,
+  watch: false,
   module: {
     rules: [
       {
